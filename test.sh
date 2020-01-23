08bb5c912ea975136bf86b21853f5e1410d11fc1
Akash Wadhawane <akash3699@gmail.com>
2020-01-24 00:43:31 +0530

    containerised

--- test.sh
+++ test.sh
@@ -7,18 +7,18 @@
 #ng build --prod
 
 # remove the container if exists or running
-if [ $(docker container ls -q --filter name=my_app_frontend_container) != '' ]; then
-    docker container stop my_app_frontend_container
-    docker container rm my_app_frontend_container
+if [ $(docker container ls -q --filter name=my_app_backend_container) != '' ]; then
+    docker container stop my_app_backend_container
+    docker container rm my_app_backend_container
 fi
 
 # remove the image if exists
-if [ $(docker image ls -q --filter reference=my_app_frontend) != '' ]; then
-    docker image rm my_app_frontend
+if [ $(docker image ls -q --filter reference=my_app_backend) != '' ]; then
+    docker image rm my_app_backend
 fi
 
 # build the image
 docker build -t my_app_frontend .
 
 # start the container
-docker run -itd -p 80:80 --name my_app_frontend_container my_app_frontend
+docker run -itd -p 8080:8080 --name my_app_backend_container my_app_backend
