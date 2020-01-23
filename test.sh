079bacc974bd704c4bdf29a9788820a4cf10cdfa
Akash Wadhawane <akash3699@gmail.com>
2020-01-24 01:04:01 +0530

    containerised

--- test.sh
+++ test.sh
@@ -8,17 +8,17 @@
 
 # remove the container if exists or running
 if [ $(docker container ls -q --filter name=my_app_backend_container) != '' ]; then
-    docker container stop my_app_backend_container
-    docker container rm my_app_backend_container
+    sudo docker container stop my_app_backend_container
+    sudo docker container rm my_app_backend_container
 fi
 
 # remove the image if exists
 if [ $(docker image ls -q --filter reference=my_app_backend) != '' ]; then
-    docker image rm my_app_backend
+    sudo docker image rm my_app_backend
 fi
 
 # build the image
-docker build -t my_app_backend .
+sudo docker build -t my_app_backend .
 
 # start the container
-docker run -itd -p 8080:8080 --name my_app_backend_container my_app_backend
+sudo docker run -itd -p 8080:8080 --name my_app_backend_container my_app_backend
