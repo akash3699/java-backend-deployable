e9f5382f3c2c90dfbd81d6e7b63e0998ab81c3b8
Akash Wadhawane <akash3699@gmail.com>
2020-01-24 00:46:28 +0530

    containerised

--- test.sh
+++ test.sh
@@ -18,7 +18,7 @@ if [ $(docker image ls -q --filter reference=my_app_backend) != '' ]; then
 fi
 
 # build the image
-docker build -t my_app_frontend .
+docker build -t my_app_backend .
 
 # start the container
 docker run -itd -p 8080:8080 --name my_app_backend_container my_app_backend
